76f6a84f52675da1ce3ecd6c952480325822fd6f
JohnVWebb <jvw1225@gmail.com>
2021-02-11 16:52:00 -0700

    updates

--- game.js
+++ game.js
@@ -1,32 +1,74 @@
-var health = 100;
+/*var health = 100;
 var dogName = "Doge"
-var hits = 0
+var hits = 0*/
+
+let dog = {
+    name:"Doge",
+    health: 100,
+    hits: 0,
+    items:[]
+}
+
+let items = {
+    bone: {
+        name: 'bone',
+        modifier: 10,
+        description: 'increases health by 10'
+    },
+    shockCollar: {
+        name: 'Shock Collar',
+        modifier: 5,
+        description: 'reduces health by 5',
+    }
+
+
+
+}
+
+function giveItem(itemKey){
+    let item = items[itemKey]
+    dog.items.push(item)
+    update()
+}
 
 function slap(){
-    health -= 1
-    hits++
+    dog.health -= 1
+    dog.hits++
     update()
 }
 
 function punch(){
-    health -= 5
-    hits++
+    dog.health -= 5
+    dog.hits++
     update()
 }
 
 function kick(){
-    health -= 10
-    hits++
+    dog.health -= 10
+    dog.hits++
     update()
 }
 
 function update(){
-    if( health < 0){
-        health = 0
+    if( dog.health < 0){
+        dog.health = 0
     }
-  document.getElementById('health').innerText = health.toString()
-  document.getElementById('name').innerText = dogName
-  document.getElementById('hits').innerText = hits.toString()
+  document.getElementById('health').innerText = dog.health.toString()
+  document.getElementById('name').innerText = dog.name
+  document.getElementById('hits').innerText = dog.hits.toString()
+  document.getElementById('items').innerText = getItemNames()
+}
+
+function getItemNames(){
+    if(dog.items.length == 0){
+        return "none"
+    }
+    let itemNames = ""
+    for (let i = 0; i < dog.items.length; i++) {
+        const item = dog.items[i];
+        itemNames += item.name + ' '
+    }
+    return itemNames.trim()
 }
 
 update()
